16f29282dd231f92a6d42453f82b880546b667e7
Igor Silva <Igor.Silva@altagenetics.com>
2023-03-11 18:57:51 -0300

    configuration startup

--- src/RegisterPeople/RegisterPeople.Application/AddressApplicationService.cs
+++ src/RegisterPeople/RegisterPeople.Application/AddressApplicationService.cs
@@ -1,6 +1,55 @@
-﻿namespace RegisterPeople.Application
+﻿using AutoMapper;
+using RegisterPeople.Application.Dtos;
+using RegisterPeople.Application.Interfaces;
+using RegisterPeople.Domain.Entitys;
+using RegisterPeople.Domain.Interfaces.Service;
+using System.Collections.Generic;
+
+namespace RegisterPeople.Application
 {
-    public class AddressApplicationService
+    public class AddressApplicationService : IAddressApplicationService
     {
+        private readonly IAddressService _addressService;
+        private readonly IMapper _mapper;
+
+        public AddressApplicationService(IAddressService addressService, IMapper mapper)
+        {
+            _addressService = addressService;
+            _mapper = mapper;
+        }
+
+        public void Add(AddressDto addressDto)
+        {
+            var address = _mapper.Map<Address>(addressDto);
+            _addressService.Add(address);
+        }
+
+        public IEnumerable<AddressDto> GetAll()
+        {
+            var addresss = _addressService.GetAll();
+            var addresssDto = _mapper.Map<IEnumerable<AddressDto>>(addresss);
+
+            return addresssDto;
+        }
+
+        public AddressDto GetById(int id)
+        {
+            var address = _addressService.GetById(id);
+            var addressDto = _mapper.Map<AddressDto>(address);
+
+            return addressDto;
+        }
+
+        public void Remove(AddressDto addressDto)
+        {
+            var address = _mapper.Map<Address>(addressDto);
+            _addressService.Remove(address);
+        }
+
+        public void Update(AddressDto addressDto)
+        {
+            var address = _mapper.Map<Address>(addressDto);
+            _addressService.Update(address);
+        }
     }
 }
--- src/RegisterPeople/RegisterPeople.Application/PersonApplicationService.cs
+++ src/RegisterPeople/RegisterPeople.Application/PersonApplicationService.cs
@@ -1,6 +1,55 @@
-﻿namespace RegisterPeople.Application
+﻿using AutoMapper;
+using RegisterPeople.Application.Dtos;
+using RegisterPeople.Application.Interfaces;
+using RegisterPeople.Domain.Entitys;
+using RegisterPeople.Domain.Interfaces.Service;
+using System.Collections.Generic;
+
+namespace RegisterPeople.Application
 {
-    public class PersonApplicationService
+    public class PersonApplicationService : IPersonApplicationService
     {
+        private readonly IPersonService _personService;
+        private readonly IMapper _mapper;
+
+        public PersonApplicationService(IPersonService personService, IMapper mapper)
+        {
+            _personService = personService;
+            _mapper = mapper;
+        }
+
+        public void Add(PersonDto personDto)
+        {
+            var person = _mapper.Map<Person>(personDto);
+            _personService.Add(person);
+        }
+
+        public IEnumerable<PersonDto> GetAll()
+        {
+            var persons = _personService.GetAll();
+            var personsDto = _mapper.Map<IEnumerable<PersonDto>>(persons);
+
+            return personsDto;
+        }
+
+        public PersonDto GetById(int id)
+        {
+            var person = _personService.GetById(id);
+            var personDto = _mapper.Map<PersonDto>(person);
+
+            return personDto;
+        }
+
+        public void Remove(PersonDto personDto)
+        {
+            var person = _mapper.Map<Person>(personDto);
+            _personService.Remove(person);
+        }
+
+        public void Update(PersonDto personDto)
+        {
+            var person = _mapper.Map<Person>(personDto);
+            _personService.Update(person);
+        }
     }
 }
